b61c560b053171573d9fbbab3e87887e4390ec2e
KrYounger <35814586+xt0fer@users.noreply.github.com>
2022-06-17 10:51:37 -0400

    Initial commit

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
@@ -0,0 +1,32 @@
+package com.zipcodewilmington.scientificcalculator;
+
+import java.util.Scanner;
+
+/**
+ * Created by leon on 2/9/18.
+ */
+public class Console {
+
+    public static void print(String output, Object... args) {
+        System.out.printf(output, args);
+    }
+
+    public static void println(String output, Object... args) {
+        print(output + "\n", args);
+    }
+
+    public static String getStringInput(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        println(prompt);
+        String userInput = scanner.nextLine();
+        return userInput;
+    }
+
+    public static Integer getIntegerInput(String prompt) {
+        return null;
+    }
+
+    public static Double getDoubleInput(String prompt) {
+        return null;
+    }
+}
---
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -0,0 +1,7 @@
+package com.zipcodewilmington.scientific_calculator;
+
+/**
+ * Created by leon on 2/9/18.
+ */
+public class TestMainApplication {
+}

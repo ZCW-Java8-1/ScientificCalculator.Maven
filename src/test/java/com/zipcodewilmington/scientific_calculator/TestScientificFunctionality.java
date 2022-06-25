be89f3ba803ac6c24e20a4a71fbb7b9e8e047804
eugeneshine1 <eugeneshine1@gmail.com>
2022-06-25 14:17:28 -0400

    values changed to radian values and answer

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestScientificFunctionality.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestScientificFunctionality.java
@@ -8,7 +8,7 @@ public class TestScientificFunctionality {
     @Test
     public void testCalcLog() {
         double expected = 0.60205999132;
-        int input = 4;
+        double input = 4;
         double actual = ScientificFunctionality.calcLog(input);
         Assertions.assertEquals(expected, actual);
     }
@@ -16,71 +16,77 @@ public class TestScientificFunctionality {
     @Test
     public void testCalcInverseLog() {
         double expected = 1000;
-        int input = 3;
+        double input = 3;
         double actual = ScientificFunctionality.calcInverseLog(input);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void testCalcNaturalLog() {
-        double expected = 0.60205999132;
-        int input = 4;
+        double expected = 1.60943791243;
+        double input = 5;
         double actual = ScientificFunctionality.calcNaturalLog(input);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void testCalcInverseNaturalLog() {
-        double expected = 0.60205999132;
-        int input = 4;
+        double expected = 20.0855369232;
+        double input = 3;
         double actual = ScientificFunctionality.calcInverseNaturalLog(input);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void testCalcSine() {
-        double expected = 0.60205999132;
-        int input = 4;
+        double expected = 0.8414709848;
+        //input in radians
+        double input = 1;
         double actual = ScientificFunctionality.calcSine(input);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void testCalcCoSine() {
-        double expected = 0.60205999132;
-        int input = 4;
+        double expected = 0.87758256189;
+        //input in radians
+        double input = 0.5;
         double actual = ScientificFunctionality.calcCoSine(input);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void testCalcTangent() {
-        double expected = 0.60205999132;
-        int input = 4;
+        double expected = 1.55740772465;
+        //input in radians
+        double input =1 ;
         double actual = ScientificFunctionality.calcTangent(input);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void testCalcInverseCosine() {
-        double expected = 0.60205999132;
-        int input = 4;
+    public void testCalcInverseSine() {
+        double expected = 0.52359877559;
+        //input in radians
+        double input = 0.5;
         double actual = ScientificFunctionality.calcInverseCosine(input);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void testCalcInverseCosine() {
-        double expected = 0.60205999132;
-        int input = 4;
+        double expected = 0;
+        //input in radians
+        double input = 1;
         double actual = ScientificFunctionality.calcInverseCosine(input);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void testCalcInverseTangent() {
-        double expected = 0.60205999132;
-        int input = 4;
+        double expected = 1.10714871779;
+        //input in radians
+        double input = 2;
         double actual = ScientificFunctionality.calcInverseTangent(input);
         Assertions.assertEquals(expected, actual);
     }

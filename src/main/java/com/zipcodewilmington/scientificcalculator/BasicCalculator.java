4d6a6f94d836cecbe371a47544dcdad937f9dad1
todd-bowers <toddbows@gmail.com>
2022-06-25 10:01:55 -0400

    created class BasicCalculator

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/BasicCalculator.java
@@ -0,0 +1,21 @@
+package com.zipcodewilmington.scientificcalculator;
+
+public class BasicCalculator {
+    String name;
+
+    public int add(int x, int y) {
+        return x + y;
+    }
+    public int subtract(int x, int y) {
+        return x - y;
+    }
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+    public int divide(int x, int y) {
+        return x / y;
+    }
+    public int modulus(int x, int y) {
+        return x % y;
+    }
+}

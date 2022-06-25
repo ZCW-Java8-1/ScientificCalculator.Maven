da58dde1a876e8133c8ed379ea3f1770d69c8660
Linda Li <limeilinda@gmail.com>
2022-06-25 16:58:40 -0400

    Multiplication and division added

--- src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -11,19 +11,22 @@ public class Calculator {
         System.out.println("Current value: " + displayVal);
 
         while (calcOn == true) {
-            String s = Console.getStringInput("Enter the operation you wish to perform: ");
+            String operator = Console.getStringInput("Enter the operation you wish to perform: ");
 
-            if (s.equals("exit")) break;
-            else if (s.equals("clear")) displayVal = 0.0;
-            else if (s.equals("+")) displayVal = Operation.twoNumOp("+", displayVal);
-            else if (s.equals("-")) displayVal = Operation.twoNumOp("-", displayVal);
+            if (operator.equals("exit")) break;
+            else if (operator.equals("clear")) displayVal = 0.0;
+            else if (operator.equals("+")) displayVal = Operation.twoNumOp("+", displayVal);
+            else if (operator.equals("-")) displayVal = Operation.twoNumOp("-", displayVal);
+            else if (operator.equals("*")) displayVal = Operation.twoNumOp("*", displayVal);
+            else if (operator.equals("/")) displayVal = Operation.twoNumOp("/", displayVal);
+            else if (operator.equals("^")) displayVal = Operation.twoNumOp("^", displayVal);
+
             else {
                 System.out.println("Invalid operation, please enter help for a list of operations");
                 continue;
             }
 
             System.out.println("Current value: " + displayVal);
-            System.out.println("test");
         }
     }
 }
--- src/main/java/com/zipcodewilmington/scientificcalculator/Operation.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Operation.java
@@ -1,8 +1,9 @@
 package com.zipcodewilmington.scientificcalculator;
-
+import java.lang.Math;
 import java.sql.SQLOutput;
 
 public class Operation {
+    // Two number operations
     public static double twoNumOp(String operation, double displayVal) {
         double num1 = 0, num2 = 0, result = 0;
 
@@ -14,7 +15,7 @@ public class Operation {
                 // Try to parse the user input into a double
                 num1 = Double.parseDouble(s1);
             } catch (NumberFormatException e){
-                System.out.println("You did not input a valid, number. The number has been set to the default value of 0.");
+                System.out.println("You did not input a valid number. The number has been set to the default value of 0.");
             }
         }
 
@@ -26,23 +27,39 @@ public class Operation {
                 // Try to parse the user input into a double
                 num2 = Double.parseDouble(s2);
             } catch (NumberFormatException e){
-                System.out.println("You did not input a valid, number. The number has been set to the default value of 0.");
+                System.out.println("You did not input a valid number. The number has been set to the default value of 0.");
             }
         }
 
         // Perform the operation
         if (operation.equals("+")) result = add(num1, num2);
         else if (operation.equals("-")) result = subtract(num1, num2);
-
+        else if (operation.equals("*")) result = multiply(num1, num2);
+        else if (operation.equals("/")) result = divide(num1, num2);
+        else if (operation.equals("^")) result = power(num1, num2);
 
         return result;
     }
 
+    // Addition
     public static double add(double num1, double num2) {
         return num1 + num2;
     }
-
+    // Subtraction
     public static double subtract(double num1, double num2) {
         return num1 - num2;
     }
+    // Multiplication
+    public static double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+    // Division
+    public static double divide(double num1, double num2) {
+        return num1 / num2;
+    }
+    // Variable exponentiation
+    public static double power(double num1, double num2) {
+        return Math.pow(num1, num2);
+    }
+
 }

682ce28390b3bb983352a3dacd394cc6bfca34e3
eugeneshine1 <eugeneshine1@gmail.com>
2022-06-25 11:45:52 -0400

    variable declared to values

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -18,35 +18,35 @@ public class TestMainApplication {
     }
     @Test
     public void testSubtraction(){
-        double expected = 3;
-        int x=12;
-        int y=9;
+        double expected = 15;
+        int x=20;
+        int y=5;
         double actual = BasicCalculator.subtract(x,y);
         Assertions.assertEquals(expected,actual);
 
     }
     @Test
     public void testMultiplication(){
-        double expected = 21;
-        int x=12;
-        int y=9;
+        double expected = 40;
+        int x=10;
+        int y=4;
         double actual = BasicCalculator.multiply(x,y);
         Assertions.assertEquals(expected,actual);
 
     }
     @Test
     public void testDivision(){
-        double expected = 21;
-        int x=12;
-        int y=9;
+        double expected = 27;
+        int x=54;
+        int y=2;
         double actual = BasicCalculator.divide(x,y);
         Assertions.assertEquals(expected,actual);
     }
     @Test
     public void testModulus(){
-        double expected = 21;
-        int x=12;
-        int y=9;
+        double expected = 0;
+        int x=50;
+        int y=5;
         double actual = BasicCalculator.modulus(x,y);
         Assertions.assertEquals(expected,actual);
     }

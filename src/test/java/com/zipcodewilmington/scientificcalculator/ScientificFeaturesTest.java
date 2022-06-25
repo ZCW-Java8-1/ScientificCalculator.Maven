d017cfe9be4b8b1c375bd19b4d522d484072d2f6
Fitru <fitru@Zipcoders-MacBook-Pro-100.local>
2022-06-25 11:37:20 -0400

    Created a new folder for ScientificFeaturesTest

---
+++ src/test/java/com/zipcodewilmington/scientificcalculator/ScientificFeaturesTest.java
@@ -0,0 +1,68 @@
+package com.zipcodewilmington.scientificcalculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScientificFeaturesTest extends Console {
+
+    @Test
+    void square() {
+    }
+
+    @Test
+    void squareRoot() {
+    }
+
+    @Test
+    void switchSign() {
+    }
+
+    @Test
+    void inverse() {
+    }
+
+    @Test
+    void sine() {
+    }
+
+    @Test
+    void cos() {
+    }
+
+    @Test
+    void tan() {
+    }
+
+    @Test
+    void inverseSine() {
+    }
+
+    @Test
+    void inverseCosine() {
+    }
+
+    @Test
+    void inverseTangent() {
+    }
+
+    @Test
+    void factorial() {
+    }
+
+    @Test
+    void log() {
+    }
+
+    @Test
+    void inverseLog() {
+    }
+
+    @Test
+    void ln() {
+    }
+
+    @Test
+    void inverseLn() {
+    }
+}

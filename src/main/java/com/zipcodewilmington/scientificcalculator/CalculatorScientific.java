25668a44b07ca461cb4330341c6f344ac9bfe1cb
Mike <mike@ZipcodersMBP41.fios-router.home>
2022-06-26 11:30:13 -0400

    started basic test cases

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/CalculatorScientific.java
@@ -0,0 +1,28 @@
+package com.zipcodewilmington.scientificcalculator;
+
+public class CalculatorScientific {
+
+    void add(int x, int y) {
+        return;
+    }
+
+    ;
+
+    void subtract(int x, int y) {
+        return;
+    }
+
+    ;
+
+    void multiply(int x, int y) {
+        return;
+    }
+
+    ;
+
+    void divide(int x, int y) {
+        return;
+    }
+
+    ;
+}

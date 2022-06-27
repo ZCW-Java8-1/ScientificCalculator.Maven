efb34eddc45dc1c4ccfb21e3dc468ecafd84eaae
Kendra <kendra@Zipcoders-MacBook-Pro-45.local>
2022-06-26 22:26:41 -0500

    added more trig tests

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestScientificFunctions.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestScientificFunctions.java
@@ -14,106 +14,152 @@ public class TestScientificFunctions {
     private ScientificCalc objSciCalcTest;
 
     @Before
-    public void setUp(){
-        objSciCalcTest = new ScientificCalc ();}
-
-    @Test
-    public void testCos(){
-        objSciCalcTest = new ScientificCalc ();
-        double randomNum = (Math.random() * 10);
-        double expectedResult = Math.cos(randomNum);
-        double result = objSciCalcTest.cos(randomNum);
-        Assert.assertEquals(expectedResult,result,0.000000d);
+    public void setUp() {
+        objSciCalcTest = new ScientificCalc();
     }
 
     @Test
-    public void testCosHard(){
-        objSciCalcTest = new ScientificCalc ();
-        double num1 = 1;
+    public void testCos() {
+        objSciCalcTest = new ScientificCalc();
+        double randomNum = (Math.random() * 10);
+        double expectedResult = Math.cos(randomNum);
+        double result = objSciCalcTest.cos(randomNum);
+        Assert.assertEquals(expectedResult, result, 0.000000d);
+    }
+
+    @Test
+    public void testCosHard() {
+        objSciCalcTest = new ScientificCalc();
         double expectedResult = Math.cos(1);
         double result = objSciCalcTest.cos(1);
-        Assert.assertEquals(expectedResult,result,0.000000d);
+        Assert.assertEquals(expectedResult, result, 0.000000d);
+    }
+
+    @Test
+    public void testCosHard2() {
+        objSciCalcTest = new ScientificCalc();
+        double expectedResult = Math.cos(2);
+        double result = objSciCalcTest.cos(2);
+        Assert.assertEquals(expectedResult, result, 0.000000d);
+    }
+    @Test
+    public void testCosNotEqual() {
+        objSciCalcTest = new ScientificCalc();
+        double expectedResult = Math.cos(1);
+        double result = objSciCalcTest.cos(2);
+        Assert.assertNotEquals(expectedResult, result, 0.000000d);
     }
 
     @Test
     public void testSine(){
-        objSciCalcTest = new ScientificCalc ();
+        objSciCalcTest = new ScientificCalc();
         double randomNum = (Math.random() * 10);
         double expectedResult = Math.sin(randomNum);
         double result = objSciCalcTest.sin(randomNum);
-        Assert.assertEquals(expectedResult,result,0.000000d);
+        Assert.assertEquals(expectedResult, result, 0.000000d);
     }
 
     @Test
-    public void testTan(){
-        objSciCalcTest = new ScientificCalc ();
+    public void testSineHard() {
+        objSciCalcTest = new ScientificCalc();
+        double expectedResult = Math.sin(2);
+        double result = objSciCalcTest.sin(2);
+        Assert.assertEquals(expectedResult, result, 0.000000d);
+    }
+    @Test
+    public void testSinNotEqual() {
+        objSciCalcTest = new ScientificCalc();
+        double expectedResult = Math.sin(1);
+        double result = objSciCalcTest.sin(2);
+        Assert.assertNotEquals(expectedResult, result, 0.000000d);
+    }
+
+    @Test
+    public void testTan() {
+        objSciCalcTest = new ScientificCalc();
         double randomNum = (Math.random() * 10);
         double expectedResult = Math.tan(randomNum);
         double result = objSciCalcTest.tan(randomNum);
-        Assert.assertEquals(expectedResult,result,0.000000d);
+        Assert.assertEquals(expectedResult, result, 0.000000d);
     }
 
     @Test
-    public void testInvSine(){
-        objSciCalcTest = new ScientificCalc ();
+    public void testTanHard() {
+        objSciCalcTest = new ScientificCalc();
+        double expectedResult = Math.tan(2);
+        double result = objSciCalcTest.tan(2);
+        Assert.assertEquals(expectedResult, result, 0.000000d);
+    }
+    @Test
+    public void testTanNotEqual() {
+        objSciCalcTest = new ScientificCalc();
+        double expectedResult = Math.tan(1);
+        double result = objSciCalcTest.tan(2);
+        Assert.assertNotEquals(expectedResult, result, 0.000000d);
+    }
+
+    @Test
+    public void testInvSine() {
+        objSciCalcTest = new ScientificCalc();
         double randomNum = Math.random();
         double expectedResult = Math.asin(randomNum);
         double result = objSciCalcTest.invSin(randomNum);
-        Assert.assertEquals(expectedResult,result,0.000000d);
+        Assert.assertEquals(expectedResult, result, 0.000000d);
     }
 
     @Test
-    public void testInvCos(){
-        objSciCalcTest = new ScientificCalc ();
+    public void testInvCos() {
+        objSciCalcTest = new ScientificCalc();
         double randomNum = Math.random();
         double expectedResult = Math.acos(randomNum);
         double result = objSciCalcTest.invCos(randomNum);
-        Assert.assertEquals(expectedResult,result,0.000000d);
+        Assert.assertEquals(expectedResult, result, 0.000000d);
     }
 
     @Test
-    public void testInvTan(){
-        objSciCalcTest = new ScientificCalc ();
+    public void testInvTan() {
+        objSciCalcTest = new ScientificCalc();
         double randomNum = Math.random();
         double expectedResult = Math.atan(randomNum);
         double result = objSciCalcTest.invTan(randomNum);
-        Assert.assertEquals(expectedResult,result,0.000000d);
+        Assert.assertEquals(expectedResult, result, 0.000000d);
     }
 
     @Test
-    public void testLog(){
-        double randomNum = (Math.random()*10);
+    public void testLog() {
+        double randomNum = (Math.random() * 10);
         double expectedResult = Math.log10(randomNum);
         double result = objSciCalcTest.log(randomNum);
-        Assert.assertEquals(expectedResult,result,0.000000d);
+        Assert.assertEquals(expectedResult, result, 0.000000d);
     }
 
     @Test
-    public void testinvLog(){
+    public void testinvLog() {
         double randomNum = (Math.random() * 10);
         double expectedResult = Math.pow(10, randomNum);
         double result = objSciCalcTest.invLog(randomNum);
-        Assert.assertEquals(expectedResult,result,0.000000d);
+        Assert.assertEquals(expectedResult, result, 0.000000d);
     }
 
     @Test
-    public void testlnFunc(){
+    public void testlnFunc() {
         double randomNum = (Math.random() * 10);
         double expectedResult = Math.log(randomNum);
         double result = objSciCalcTest.natLog(randomNum);
-        Assert.assertEquals(expectedResult,result,0.000000d);
+        Assert.assertEquals(expectedResult, result, 0.000000d);
     }
 
     @Test
-    public void testinvLnFunc(){
+    public void testinvLnFunc() {
         double randomNum = (Math.random() * 10);
         //I don't know if this is the right math method
         double expectedResult = Math.pow(Math.E, randomNum);
         double result = objSciCalcTest.invNatLog(randomNum);
-        Assert.assertEquals(expectedResult,result,0.000000d);
+        Assert.assertEquals(expectedResult, result, 0.000000d);
     }
+
     @Test
-    public void abs(){
+    public void abs() {
         double testNum = 8;
         double expectedResult = (Math.abs(testNum));
         double result = objSciCalcTest.abs(testNum);
@@ -121,7 +167,7 @@ public class TestScientificFunctions {
     }
 
     @Test
-    public void factorial(){
+    public void factorial() {
         long testNum = 8;
         long expectedResult = 40320;
         long result = objSciCalcTest.factorial(testNum);
@@ -129,35 +175,21 @@ public class TestScientificFunctions {
     }
 
     @Test
-    public void factorialFail(){
+    public void factorialFail() {
         long testNum = 8;
         long expectedResult = 94032;
         long result = objSciCalcTest.factorial(3);
         Assert.assertNotEquals(expectedResult, result);
     }
+
     @Test
-    public void Pi(){
+    public void Pi() {
         long expectedResult = (long) Math.PI;
         long result = (long) objSciCalcTest.pi();
         Assert.assertEquals(expectedResult, result, 0.00000000d);
     }
+}
 
-//    @Test
-//    public void testMemoryStoreDefault(){
-//        Assertions.assertEquals(0,calcMemoryMethodDefault);
-//        (M+ key) Add the currently displayed value to the value in memory (store in memory and update display) *
-//        (MC key) Reset memory *
-//        (MRC key) Recall the current value from memory to the display
-    }
-
-  //  @Test
-  //  public void testSwitchDisplayMode(){
-  //          Assertions.assertEquals(boolean, octal, float, double)
-  //      }
-
-//    Switch trig units mode (Degrees, Radians)
- //   switchUnitsMode() should rotate through the options
-  //  switchUnitsMode(String mode) should set the trig units to the type given
 
 
 

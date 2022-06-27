ad25dd056e0d8d0e41abe98ad12f79fe91732e04
Fitru <fitru@ZipcodersMBP100.fios-router.home>
2022-06-26 20:28:59 -0400

    Everything is back to normal

--- src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
@@ -39,119 +39,15 @@ public class Console {
             System.out.print("Enter first number ");
             firstNumber = Calculator.nextDouble();
 
+
+            System.out.println("Enter math operator to use for this calculation");
+            operator = Calculator.next();
+
+            System.out.print("Enter second number ");
+            secondNumber = Calculator.nextDouble();
+
             // Attempt to handle the error, but need a try/catch
-            if (firstNumber != answer) {
-                System.out.println("Incorrect operator");
-                do {
-                    System.out.println("Do you want to exit? \nPlease enter Y or press N to clear the display");
-                    String inputEscRead = inputEsc.next();
-                    if (inputEscRead.equalsIgnoreCase("Y")) {
-                        onButton = false;
-                    } else if (inputEscRead.equalsIgnoreCase("N")) {
-                        Console reRun = new Console();
-                        clear.clear();
-                        reRun.calcRun();
-                        wrongOperator = false;
-                    } else {
-                        System.out.println("Please enter the correct letter");
-                    }
-                    break;
-                } while (wrongOperator);
 
-                // print calculator choice
-                System.out.print("Enter math operator to use for this calculation ");
-                operator = Calculator.next();
-
-                // Attempt to handle the error, but need a try/catch
-                if (!operator.equals("+/-") || !operator.equals("+") || !operator.equals("-") || !operator.equals("/") || !operator.equals("*/") ||
-                        !operator.equals("*") || !operator.equals("**") || !operator.equals("fac") || !operator.equals("sine") || !operator.equals("cosine") ||
-                        !operator.equals("tan") || !operator.equals("invsine") || !operator.equals("incosine") || !operator.equals("invtangent")) {
-                    System.out.println("Incorrect operator");
-                    do {
-                        System.out.println("Do you want to exit? \nPlease enter Y or press N to clear the display");
-                        String inputEscRead = inputEsc.next();
-                        if (inputEscRead.equalsIgnoreCase("Y")) {
-                            onButton = false;
-                        } else if (inputEscRead.equalsIgnoreCase("N")) {
-                            Console reRun = new Console();
-                            clear.clear();
-                            reRun.calcRun();
-                        } else {
-                            System.out.println("Please enter the correct letter");
-                        }
-                        break;
-                    } while (!wrongOperator);
-
-                    // check if the operator sees a sign
-                    if (operator.equalsIgnoreCase("+/-")) {
-                        String switchSignFomrulaRead = String.valueOf((int) switchSignFormula.switchSign(firstNumber));
-                        switchSignFormulaHolder = switchSignFomrulaRead;
-                        System.out.print("Enter second number ");
-                        secondNumber = Calculator.nextDouble();
-                    }
-                    System.out.println("Enter math operator to use for this calculation (if another negative number, please enter +/- again)");
-                    operator = Calculator.next();
-                    // Need a try catch method
-                    if (!operator.equals("+/-") || !operator.equals("+") || !operator.equals("-") || !operator.equals("/") || !operator.equals("*/") ||
-                            !operator.equals("*") || !operator.equals("**") || !operator.equals("fac") || !operator.equals("sine") || !operator.equals("cosine") ||
-                            !operator.equals("tan") || !operator.equals("invsine") || !operator.equals("incosine") || !operator.equals("invtangent")) {
-                        System.out.println("Incorrect operator");
-                        do {
-                            System.out.println("Do you want to exit? \nPlease enter Y or press N to clear the display");
-                            String inputEscRead = inputEsc.next();
-                            if (inputEscRead.equalsIgnoreCase("Y")) {
-                                onButton = false;
-                            } else if (inputEscRead.equalsIgnoreCase("N")) {
-                                clear.clear();
-                            } else {
-                                System.out.println("Please enter the correct letter");
-                            }
-                            break;
-                        } while (!wrongOperator);
-                    }
-                    // operator sees a sign
-                    if (operator.equalsIgnoreCase("+/-")) {
-                        String switchSignFormulaReadSecond = String.valueOf((int) switchSignFormula.switchSign((secondNumber)));
-                        switchSignFormulaHolderNegSecond = switchSignFormulaReadSecond;
-                        System.out.println("Enter math operator to use for this calculation");
-                        operator = Calculator.next();
-                        // operator types in a plus, additoin
-                        if (operator.equalsIgnoreCase("+")) {
-                            CoreFeatures additionFormula = new CoreFeatures();
-                            String additionFormulaRead = String.valueOf((int) additionFormula.add(Double.parseDouble(switchSignFormulaHolder), Double.parseDouble(switchSignFormulaHolderNegSecond)));
-                            System.out.println(additionFormulaRead);
-                            System.out.println("Do you want to exit? \n Please enter Y or press N to clear the display");
-                            String inputEscRead = inputEsc.next();
-                            if (inputEscRead.equalsIgnoreCase("Y")) {
-                                onButton = false;
-                            } else if (inputEscRead.equalsIgnoreCase("N")) {
-                                System.out.println("\033[H\033[2J");
-                                System.out.flush();
-                            } else {
-                                System.out.println("Please enter the correct letter");
-                            }
-
-                        } else if (operator.equalsIgnoreCase("-")) {
-                            CoreFeatures subtractionFormula = new CoreFeatures();
-                            String subtractionFormulaRead = String.valueOf((int) subtractionFormula.subtract(Double.parseDouble(switchSignFormulaHolder), Double.parseDouble(switchSignFormulaHolderNegSecond)));
-                            System.out.println(subtractionFormulaRead);
-                            System.out.println("Do you want to exit? \n Please enter Y or press N to clear the display");
-                            String inputEscRead = inputEsc.next();
-                            if (inputEscRead.equalsIgnoreCase("Y")) {
-                                onButton = false;
-                            } else if (inputEscRead.equalsIgnoreCase("N")) {
-                                clear.clear();
-                            } else {
-                                System.out.println("Please enter the correct letter");
-                            }
-                        }
-                    }
-                } else {
-                    System.out.println("Enter math operator to use for this calculation");
-                    operator = Calculator.next();
-                    System.out.print("Enter second number ");
-                    secondNumber = Calculator.nextDouble();
-                }
 
 
                 switch (operator) {
@@ -263,7 +159,7 @@ public class Console {
         }
 
     }
-}
+
 
 
 

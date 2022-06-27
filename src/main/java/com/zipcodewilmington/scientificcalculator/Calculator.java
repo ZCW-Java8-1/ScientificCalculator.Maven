e362054de29877b6c936f2f472a428c6760038c2
jjheffernan <jjheffernan99@gmail.com>
2022-06-26 22:28:01 -0400

    all basic operators within test case

--- src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -5,54 +5,118 @@ import java.lang.Math;
 
 public class Calculator {
     Double x, y, mem, result;
-    Console choiceString = new Console();
+    String ans;
+    String choiceString = "+";
     // MainApplication activeCalc = new MainApplication();
 
     // Input section
+    public void Arithmetic(String[] args) {
+        switch (choiceString) {
+            // Case 1
+            case "+":
+                // Print statement corresponding case
 
+                // break keyword terminates the
+                result = add(x,y);
+                System.out.println("one");
+                break;
+            // Case 2
+            case "-":
+                // Print statement corresponding case
+                result = subtract(x,y);
+                System.out.println("two");
+                break;
+
+            // Case 3
+            case "*":
+                // Print statement corresponding case
+                result = multiply(x,y);
+                System.out.println("three");
+                break;
+            // Case 4
+            case "/":
+                // Print statement corresponding case
+                result = divide(x,y);
+                System.out.println("four");
+                break;
+            case "square":
+                // Print statement corresponding case
+                result = sqr(x,y);
+                System.out.println("five");
+                break;
+            case "sqrt":
+                // Print statement corresponding case
+                result = root(x,y);
+                System.out.println("six");
+                break;
+            case "1/x":
+                // Print statement corresponding case
+                result = inverse(x,y);
+                System.out.println("seven");
+                break;
+            case "+/-":
+                // Print statement corresponding case
+                result = invertSign(x,y);
+                System.out.println("eight");
+                break;
+            case "x^y":
+                // Print statement corresponding case
+                result = sqrY(x,y);
+                System.out.println("eight");
+                break;
+            default:
+
+                // Print statement corresponding case
+                System.out.println("Err, no match");
+            break;
+        }
+        ans = String.format(String.valueOf(result));
+
+    }
 
 
     // Operator Choice
+
 	// System.out.println("Choose an operator from '+' '-' '*' '/' '^sq' '^exp' '^y' '^neg 'inv'");
 
-    public int add(int x, int y) {
+    public double add(double x, double y) {
         return x + y;
     }
 
-    public int subtract(int x, int y) {
+    public double subtract(double x, double y) {
         return x - y;
     }
 
-    public int multiply(int x, int y) {
+    public double multiply(double x, double y) {
         return x * y;
     }
 
-    public int divide(int x, int y) {
+    public double divide(double x, double y) {
         return x / y;
     }
 
-    public int sqr(int x, int y) {
-        y = (int) Math.pow(x,2);
+    public double sqr(double x, double y) {
+        y = Math.pow(x,2);
         return y ;  //
     }
 
-    public int sqry(int x, int y, int result) {
-        result = (int) Math.pow(x,y);
+    public double sqrY(double x, double y) {
+        double result = Math.pow(x, y);
         return result;
     }
 
-    public int root(int x, int y) {
-        y = (int) Math.sqrt(x);
+    public double root(double x, double y) {
+        y = Math.sqrt(x);
         return y;
     }
 
-    public int inverse(int x, int y) {
-        y = (int) Math.expm1(x);
+    public double inverse(double x, double y) {
+        y = Math.expm1(x);
         return y;
     }
 
-    public int invertSign(int x, int y) {
-        y = (int) x*-1 ;
+    public double invertSign(double x, double y) {
+        y = x*-1;
         return y;
     }
 
